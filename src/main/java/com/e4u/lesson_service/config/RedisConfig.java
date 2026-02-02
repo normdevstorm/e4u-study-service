f34d14458bd247dd6975f30240f389337021ddd0
normdevstorm <nllsover8.0@gmail.com>
2026-02-02 22:03:55 +0700

    feat: configure logging for service

--- src/main/java/com/e4u/lesson_service/config/RedisConfig.java
+++ src/main/java/com/e4u/lesson_service/config/RedisConfig.java
@@ -3,7 +3,6 @@ package com.e4u.lesson_service.config;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -15,29 +14,29 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory,
+            ObjectMapper objectMapper) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
-        
+
         // Use String serializer for keys
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-        
-        // Configure ObjectMapper with type info for polymorphic serialization
-        ObjectMapper objectMapper = JsonMapper.builder()
-                .addModule(new JavaTimeModule())
+
+        // Configure ObjectMapper copy with type info for Redis polymorphic
+        // serialization
+        ObjectMapper redisObjectMapper = objectMapper.copy()
                 .activateDefaultTyping(
                         JsonMapper.builder().build().getPolymorphicTypeValidator(),
                         ObjectMapper.DefaultTyping.NON_FINAL,
-                        JsonTypeInfo.As.PROPERTY
-                )
-                .build();
-        
+                        JsonTypeInfo.As.PROPERTY);
+
         // Use Jackson2JsonRedisSerializer for values
-        Jackson2JsonRedisSerializer<Object> jsonSerializer = new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
+        Jackson2JsonRedisSerializer<Object> jsonSerializer = new Jackson2JsonRedisSerializer<>(redisObjectMapper,
+                Object.class);
         template.setValueSerializer(jsonSerializer);
         template.setHashValueSerializer(jsonSerializer);
-        
+
         template.afterPropertiesSet();
         return template;
     }
